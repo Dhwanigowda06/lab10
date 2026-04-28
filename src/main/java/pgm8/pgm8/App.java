04e04d1c965b724c581c935926047dd0e745f06c
Dhwanigowda06 <dhwanigowda2005@gmail.com>
2026-04-28 11:27:02 +0530

    commit

---
+++ src/main/java/pgm8/pgm8/App.java
@@ -0,0 +1,13 @@
+package pgm8.pgm8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
